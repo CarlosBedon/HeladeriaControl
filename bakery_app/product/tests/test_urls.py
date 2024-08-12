91dea6ca6c96fd8846d1b9f71f0a9f9d12564659
CARLOS BEDON <carlosbedon@outlook.com>
2024-08-11 22:38:00 -0500

    Test URL 100%

--- bakery_app/product/tests/test_urls.py
+++ bakery_app/product/tests/test_urls.py
@@ -19,3 +19,10 @@ def test_update():
     product = Product.objects.create(presentacion="Cono", peso="150", precio="1.10")
     assert reverse("product:update", kwargs={"pk": product.pk}) == f"/product/update/{product.pk}/"
     assert resolve(f"/product/update/{product.pk}/").view_name == "product:update"
+
+
+@pytest.mark.django_db
+def test_delete():
+    product = Product.objects.create(presentacion="Cono", peso="150", precio="1.10")
+    assert reverse("product:delete", kwargs={"pk": product.pk}) == f"/product/delete/{product.pk}/"
+    assert resolve(f"/product/delete/{product.pk}/").view_name == "product:delete"
