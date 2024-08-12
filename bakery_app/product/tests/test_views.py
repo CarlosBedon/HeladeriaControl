fd3d3e975c5fa2b95ca041a77035bb87a192c651
CARLOS BEDON <carlosbedon@outlook.com>
2024-08-11 23:51:25 -0500

    Add test_urls.py TestProductCreate

--- bakery_app/product/tests/test_views.py
+++ bakery_app/product/tests/test_views.py
@@ -3,26 +3,45 @@ from django.urls import reverse
 
 from bakery_app.product.models import Product
 
-# from bakery_app.product.views import ProductView
 
+@pytest.mark.django_db
+class TestProductView:
+    @pytest.fixture
+    def data_test(self):
+        def _data_test():
+            product1 = Product.objects.create(presentacion="Waffle", peso="150", precio="1.10")
+            product2 = Product.objects.create(presentacion="Cono Doble", peso="200", precio="2.10")
+            return product1, product2
 
-@pytest.fixture
-def data_test():
-    def _data_test():
-        product1 = Product.objects.create(presentacion="Waffle", peso="150", precio="1.10")
-        product2 = Product.objects.create(presentacion="Cono Doble", peso="200", precio="2.10")
-        return product1, product2
+        return _data_test
 
-    return _data_test
-
-
-class Test_Product_View:
     def test_get_products(self, client, data_test):
         data = data_test()
 
-        assert data.product1.presentacion == "Waffle"
-        assert data.product2.presentacion == "Cono Doble"
+        assert str(data[0]) == "Waffle"
+        assert str(data[1]) == "Cono Doble"
         response = client.get(reverse("product:product"))
         assert response.status_code == 200
         assert "Cono Doble" in str(response.content)
         assert "Waffle" in str(response.content)
+
+
+@pytest.mark.django_db
+class TestProductCreate:
+    @pytest.fixture
+    def data_test(self):
+        def _data_test():
+            product_new = Product.objects.create(presentacion="Cono Doble", peso="180", precio="1.50")
+            return product_new
+
+        return _data_test
+
+    def test_create_products(self, client, data_test):
+        data = data_test()
+
+        assert str(data[0]) == "Cono Doble"
+        response = client.get(reverse("product:create"))
+        assert response.status_code == 200
+        assert "Tipo de Producto" in str(response.content)
+        assert "Precio" in str(response.content)
+        assert "Peso" in str(response.content)
