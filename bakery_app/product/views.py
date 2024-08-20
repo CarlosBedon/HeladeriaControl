18c2b9e3c7a02a98811e38fa5117a2ee26d2d267
CARLOS BEDON <carlosbedon@outlook.com>
2024-08-20 02:40:19 -0500

    Modify Staff Required Mixin with decorator

--- bakery_app/product/views.py
+++ bakery_app/product/views.py
@@ -1,6 +1,7 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django_filters.views import FilterView
 from django_tables2 import SingleTableMixin
@@ -12,9 +13,8 @@ from .tables import FlavoursTable, ProductTable
 
 
 class StaffRequiredMixin:
+    @method_decorator(staff_member_required)
     def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_staff:
-            return redirect(reverse_lazy("admin:login"))
         return super().dispatch(request, *args, **kwargs)
 
 
