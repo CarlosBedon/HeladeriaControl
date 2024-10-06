a6752e6f387b7ef4e66ed566758d020b82a75057
CARLOS BEDON <carlosbedon@outlook.com>
2024-10-06 13:00:23 -0500

    Test Model Passed

--- bakery_app/product/tests/test_models.py
+++ bakery_app/product/tests/test_models.py
@@ -2,25 +2,25 @@
 
 import pytest
 
-from bakery_app.product.models import Flavours, Product
+from bakery_app.product.models import FlavoursIceCream, MenuHeladeria
 
 
 @pytest.mark.django_db
 def test_create_product(products_create):
-    productos = Product.objects.all()
+    productos = MenuHeladeria.objects.all()
     assert productos.count() > 0
 
 
 @pytest.mark.django_db
 def test_create_product_positive_price_show_raisen_error(products_create):
-    producto = Product.objects.all()
+    producto = MenuHeladeria.objects.all()
     for price in producto:
         assert float(price.precio) > 0.0
 
 
 @pytest.mark.django_db
 def test_create_date_update_date(products_create):
-    producto = Product.objects.all()
+    producto = MenuHeladeria.objects.all()
     for t in producto:
         print(t.created)
         assert t.created <= t.updated
@@ -28,5 +28,5 @@ def test_create_date_update_date(products_create):
 
 @pytest.mark.django_db
 def test_create_flavour(flavours_create):
-    flavour = Flavours.objects.all()
+    flavour = FlavoursIceCream.objects.all()
     assert flavour.count() > 0
