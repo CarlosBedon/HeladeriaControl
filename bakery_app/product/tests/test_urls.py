31ddbfea7272fc776d92041bb1d859a162b9300e
CARLOS BEDON <carlosbedon@outlook.com>
2024-10-06 12:55:32 -0500

    Test URL Passed

--- bakery_app/product/tests/test_urls.py
+++ bakery_app/product/tests/test_urls.py
@@ -1,7 +1,7 @@
 import pytest
 from django.urls import resolve, reverse, reverse_lazy
 
-from bakery_app.product.models import Flavours, Product
+from bakery_app.product.models import FlavoursIceCream, MenuHeladeria
 
 
 def test_product_view():
@@ -16,14 +16,14 @@ def test_product_create():
 
 @pytest.mark.django_db
 def test_product_update(products_create):
-    product = Product.objects.first()
+    product = MenuHeladeria.objects.first()
     assert reverse("product:update", kwargs={"pk": product.pk}) == f"/product/update/{product.pk}/"
     assert resolve(f"/product/update/{product.pk}/").view_name == "product:update"
 
 
 @pytest.mark.django_db
 def test_product_delete(products_create):
-    product = Product.objects.first()
+    product = MenuHeladeria.objects.first()
     assert reverse("product:delete", kwargs={"pk": product.pk}) == f"/product/delete/{product.pk}/"
     assert resolve(f"/product/delete/{product.pk}/").view_name == "product:delete"
 
@@ -40,13 +40,13 @@ def test_flavour_create():
 
 @pytest.mark.django_db
 def test_flavour_update(flavours_create):
-    flavour = Flavours.objects.first()
+    flavour = FlavoursIceCream.objects.first()
     assert reverse("product:flavourUpdate", kwargs={"pk": flavour.pk}) == f"/product/flavour/update/{flavour.pk}/"
     assert resolve(f"/product/flavour/update/{flavour.pk}/").view_name == "product:flavourUpdate"
 
 
 @pytest.mark.django_db
 def test_flavour_delete(flavours_create):
-    flavour = Flavours.objects.first()
+    flavour = FlavoursIceCream.objects.first()
     assert reverse("product:flavourDelete", kwargs={"pk": flavour.pk}) == f"/product/flavour/delete/{flavour.pk}/"
     assert resolve(f"/product/flavour/delete/{flavour.pk}/").view_name == "product:flavourDelete"
